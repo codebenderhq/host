6615a96854ee4137dd4dd51586e6e3fb15b16ba0
codebender <lionelmbankeu@gmail.com>
2022-12-29 23:53:37 +0200

    can i not use unstable things

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import "https://deno.land/std@0.170.0/dotenv/load.ts";
-//import { serve, serveTls } from "https://deno.land/std@0.167.0/http/server.ts";
-//
-//
-//console.log(Deno.env.get('PORT'))
-//
-//const service = () => {
-//    return new Response("Hello, world")
-//}
-//
-//serve(service, {port:Deno.env.get('PORT')})
-Deno.serve({ port: Deno.env.get('PORT') }, (_req) => new Response("Hello, world"));
+import { serve, serveTls } from "https://deno.land/std@0.167.0/http/server.ts";
+
+
+console.log(Deno.env.get('PORT'))
+
+const service = () => {
+    return new Response("Hello, world")
+}
+
+serve(service, {port:Deno.env.get('PORT')})
+//Deno.serve({ port: Deno.env.get('PORT') }, (_req) => new Response("Hello, world"));
