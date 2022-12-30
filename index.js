2e52d4cb073027a5db2a3456d5dbbfafea15f792
codebender <lionelmbankeu@gmail.com>
2022-12-30 18:24:10 +0200

    one step

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import "https://deno.land/std@0.170.0/dotenv/load.ts";
 import { serve, serveTls } from "https://deno.land/std@0.170.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.170.0/http/file_server.ts";
  
 const port = Deno.env.get('ENV') ?  9000 : 80
 
-const service = () => {
+const service = (req) => {
+    console.log(req)
     return new Response("Hello, Dev")
 }
 
