fee978ae1cfd6f6497e174c34de87c5b2121ae3a
codebender <lionelmbankeu@gmail.com>
2022-12-30 17:30:25 +0200

    testing some dev

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import "https://deno.land/std@0.170.0/dotenv/load.ts";
 import { serve, serveTls } from "https://deno.land/std@0.170.0/http/server.ts";
  
 const port = Deno.env.get('ENV') ?  9000 : 80
+
 const service = () => {
-    return new Response("Hello, worlds")
+    return new Response("Hello, world")
 }
 
 serve(service, {port})
 
- 
+
 //we will test this again when we have certs
 //Deno.serve({ port: Deno.env.get('PORT') }, (_req) => new Response("Hello, world"));
