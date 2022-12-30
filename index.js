455ff2f2ebc032a11c3f7af92625eb82ff0f77d8
codebender <lionelmbankeu@gmail.com>
2022-12-30 17:28:45 +0200

    interesting no deployment would happen

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const service = () => {
 
 serve(service, {port})
 
-
-//were going to have to create a file
+ 
 //we will test this again when we have certs
 //Deno.serve({ port: Deno.env.get('PORT') }, (_req) => new Response("Hello, world"));
