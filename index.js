bdfe908cd77a8e03d43abce9c16b3b82ab496ebc
codebender <lionelmbankeu@gmail.com>
2022-12-30 10:27:55 +0200

    should serve now at port 80

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import "https://deno.land/std@0.170.0/dotenv/load.ts";
-import { serve, serveTls } from "https://deno.land/std@0.167.0/http/server.ts";
+import { serve, serveTls } from "https://deno.land/std@0.170.0/http/server.ts";
  
+const port = Deno.env.get('ENV') ?  9000 : 80
 const service = () => {
     return new Response("Hello, worlds")
 }
 
-serve(service, {port:Deno.env.get('PORT')})
+serve(service, {port})
 
 //we will test this again when we have certs
 //Deno.serve({ port: Deno.env.get('PORT') }, (_req) => new Response("Hello, world"));
