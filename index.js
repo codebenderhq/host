8cf7aa8ee74b61f19fc812fd225e6b359d94073d
codebender <lionelmbankeu@gmail.com>
2022-12-31 00:59:44 +0200

    added init bare repo

--- index.js
+++ index.js
@@ -2,42 +2,58 @@ import "https://deno.land/std@0.170.0/dotenv/load.ts";
 import { serve, serveTls } from "https://deno.land/std@0.170.0/http/server.ts";
 import { serveFile } from "https://deno.land/std@0.170.0/http/file_server.ts";
 
-const isDev = Deno.env.get("env") === 'dev'
+const isDev = Deno.env.get("env") === "dev";
 const port = isDev ? 9000 : 443;
-const certFile = isDev ? "./space/host.cert" : "/etc/letsencrypt/live/space.sauveur.xyz/fullchain.pem";
-const keyFile = isDev ? "./space/host.key" : "/etc/letsencrypt/live/space.sauveur.xyz/privkey.pem";
+const certFile = isDev
+  ? "./space/host.cert"
+  : "/etc/letsencrypt/live/space.sauveur.xyz/fullchain.pem";
+const keyFile = isDev
+  ? "./space/host.key"
+  : "/etc/letsencrypt/live/space.sauveur.xyz/privkey.pem";
 
 const options = {
   port,
   certFile,
-  keyFile
+  keyFile,
 };
 
 const service = async (req, ifo) => {
   const { pathname } = new URL(req.url);
-  const host = req.headers.get('host');
+  const host = req.headers.get("host");
 
+  const appPath = host === "space.sauveur.xyz" || host === "localhost:9000"
+    ? `${pathname}.dev`
+    : `/${host}`;
+  const appFolder = `${isDev ? Deno.cwd() : "/apps/home"}${appPath}`;
 
-  const appPath = host === 'space.sauveur.xyz' || host === 'localhost:9000' ? `${pathname}.dev` : `/${host}`;
-
+  console.log(appFolder);
 
   if (pathname.includes(".well-known")) {
-      return serveFile(req, `/apps${pathname}`);
+    return serveFile(req, `/apps${pathname}`);
   }
 
+  if (pathname.includes(".init")) {
+    // define command used to create the subprocess
+    const cmd = ["git", "init", "--bare", `${appFolder}`];
 
-  try{
-      const app = await import(`${isDev ? Deno.cwd() : '/apps/home'}${appPath}/index.js`);
+    // create subprocess
+    const p = Deno.run({ cmd });
 
-      //import app middeware to serve      
-      console.log(app);
-      return new Response("A new dawn is upon us");
-  }catch{
-      return new Response("You look lost, Happy New Year");
+    // await its completion
+    await p.status();
+
+    return new Response(`repo init at ${appFolder}`);
   }
 
+  try {
+    const app = await import(`${appFolder}/index.js`);
 
-
+    //import app middeware to serve
+    console.log(app);
+    return new Response("A new dawn is upon us");
+  } catch {
+    return new Response("You look lost, Happy New Year");
+  }
 };
 
 //serve(service, {port})
