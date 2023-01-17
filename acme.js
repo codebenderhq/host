72b482916d14bbdbda33b85144ab235e3be98699
codebender <lionelmbankeu@gmail.com>
2023-01-17 22:11:33 +0200

    this is assuming host has something to say

--- acme.js
+++ acme.js
@@ -16,7 +16,7 @@ const service = async (req, info) => {
         return new Response(null, {
             status: 301,
             headers:{
-                Location: `https://${host.replace('www.','')}`
+                Location: `https://${host.replace('www.','')}${pathname.replace('/','')}`
             }
         })
     }
