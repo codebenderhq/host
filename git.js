c2cc403cd04175bb28a72c825d9a450a4fc0d1fe
codebender <lionelmbankeu@gmail.com>
2023-01-22 22:43:47 +0200

    we did something good

--- git.js
+++ git.js
@@ -4,16 +4,21 @@ self.onmessage = async (e) => {
     
     console.log('new git process starting')
 
-    const { host, isDev } = e.data;
-    const cmd = [`${isDev ? './space/' : '/apps/home/'}serve.bash`, `${isDev ? './': '/apps/home/' }${host}`];
-    // create subprocess
-    
-    const p = Deno.run({ cmd });
-    
-    // await its completion
-    await p.status();
- 
-    self.close();
+    try{
+        const { host, isDev } = e.data;
+        const cmd = [`${isDev ? './space/' : '/apps/space/'}serve.bash`, `${isDev ? './': '/apps/home/' }${host}`];
+        // create subprocess
+        
+        const p = Deno.run({ cmd });
+        
+        // await its completion
+        await p.status();
+     
+        self.close();
+    }catch (err){
+        console.log(err)
+    }
+   
   };
 
  
