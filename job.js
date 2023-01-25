3cc9dae02f03ae18f122635fa1a9c7bba778db10
codebender <lionelmbankeu@gmail.com>
2023-01-25 20:01:01 +0200

    to begin the job

--- job.js
+++ job.js
@@ -1,5 +1,13 @@
 
-console.log('job worker created')
+console.log('job worker created', Deno.env.get('env'))
+
 setInterval(()=> {
     console.log('running the jobs for today', new Date().toTimeString())
 },86400000)
+
+
+if(Deno.env.get('env')){
+    setInterval(()=> {
+        console.log('running the jobs for today', new Date().toTimeString())
+    },86400)
+}
