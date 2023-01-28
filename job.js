2510dbcaaa588d9647629c1111f43c18a07c0486
rawkakani <lionelmbankeu@gmail.com>
2023-01-28 16:57:25 +0200

    adding job logic

--- job.js
+++ job.js
@@ -7,7 +7,25 @@ setInterval(()=> {
 
 
 if(Deno.env.get('env')){
-    setInterval(()=> {
-        console.log('running the jobs for today', new Date().toTimeString())
-    },86400)
+    setInterval(async ()=> {
+        const {default:api} = await import('app.sauveur.dev/src/_app/db/api/get.js')
+        const {default:date} = await import('app.sauveur.dev/src/_app/job/api/services/index.js')
+        const res = await api(new Request(`http://hello.com?col=job&indexName=date&index=${date.getToday()}`,{
+            headers:{
+                referer: 'app.sauveur.xyz'
+            }
+        }))
+
+        if(res.status === 200){
+            const activeJobs = res.document_data.filter(data => data.active && data.duration > 0)
+            console.log(activeJobs, 'jobs for today')
+            activeJobs.forEach(element => {
+                  // payload request
+                  console.log(element)
+                  // if payload succeful 
+                  // update next date of job
+            
+            });
+        }
+    },8640)
 }
