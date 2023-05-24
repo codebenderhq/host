0fea942217437374f8f80fffec73abfd721e72a7
rawk akani <lionelmbankeu@gmail.com>
2023-05-24 16:58:19 +0200

    updated std

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "media",
   content: [
     "./app/**/*.{html,js,svg,css,jsx}",
     "/apps/**/*.{html,js,svg,css,jsx}",
- 
 ],
   theme: {
     extend: {},
